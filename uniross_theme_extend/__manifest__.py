41b38cf9b60defa2d103bd12c9bb7a66de8ebd22
Pragnadeep Pitroda <pragnadeep@caretit.com>
2024-07-17 15:24:53 +0530

    [ADD] uniross_theme_extend: Added custom module for theme related modifications.

---
+++ uniross_theme_extend/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#                                                                            #
+# Part of Caret IT Solutions Pvt. Ltd. (Website: www.caretit.com).           #
+# See LICENSE file for full copyright and licensing details.                 #
+#                                                                            #
+##############################################################################
+
+{
+    'name': 'Theme Extended',
+    'version': '1.0',
+    'license': 'OPL-1',
+    'summary': 'Theme Extended',
+    'category': 'Website',
+    'description': """ Theme modification """,
+    'author': 'Caret IT Solutions Pvt. Ltd.',
+    'website': 'http://www.caretit.com',
+    'depends': ['website'],
+    'data': [
+        'views/website_menu.xml',
+        'views/website_templates.xml',
+    ],
+}
---
+++ uniross_theme_extend/models/website_menu.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#                                                                            #
+# Part of Caret IT Solutions Pvt. Ltd. (Website: www.caretit.com).           #
+# See LICENSE file for full copyright and licensing details.                 #
+#                                                                            #
+##############################################################################
+
+from odoo import fields, models
+
+
+class WebsiteMenu(models.Model):
+    _inherit = 'website.menu'
+
+    menu_image = fields.Image()
